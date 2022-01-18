a95fda7cb0d9db316de7feb64a450766a64550dd
Trajan Miller <trajanmiller324@gmail.com>
2022-01-17 20:14:55 -0500

    Draw a 16x16 grid and add grid squares to grid on mouse over

---
+++ app.js
@@ -0,0 +1,22 @@
+const gridContainer = document.querySelector(".grid");
+
+const drawGrid = () => {
+  gridContainer.style.width = "960px";
+  gridContainer.style.height = "700px";
+  gridContainer.style.display = "grid";
+  gridContainer.style.gridTemplateRows = "repeat(16, 1fr)";
+  gridContainer.style.gridTemplateColumns = "repeat(16, 1fr)";
+  gridContainer.style.border = "2px solid black";
+
+  for (let i = 0; i < 16 * 16; i++) {
+    let gridSquares = document.createElement("div");
+
+    gridSquares.addEventListener("mouseover", () => {
+      gridSquares.classList.add("square");
+    });
+
+    gridContainer.appendChild(gridSquares);
+  }
+};
+
+drawGrid();
